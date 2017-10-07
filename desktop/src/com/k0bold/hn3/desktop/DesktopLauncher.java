ce1a28a09f01dfcbbc76c043195ae6b7f7c9fdfc
k0bold <elzilchothe0@gmail.com>
2017-10-07 08:37:03 -0400

    initial commit

---
+++ desktop/src/com/k0bold/hn3/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.k0bold.hn3.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.k0bold.hn3.HalloweenNight3;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HalloweenNight3(), config);
+	}
+}
